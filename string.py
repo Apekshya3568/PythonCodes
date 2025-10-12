caea0dfaa77c84df5ad90ab60bd42fbe71c70c28
Apekshya3568 <apekshyalamichhane30@gmail.com>
2025-10-12 11:55:09 +0545

    Add string.py file

---
+++ string.py
@@ -0,0 +1,27 @@
+# string 
+"""
+name = "Apekshya"
+print(name)
+"""
+
+# indexing and slicing 
+"""
+print(name[0])
+print(name[:4])
+print(name[0:])
+print(name[-4:-1])
+"""
+
+# striing methods
+color = "this is the red color which is my fav color"
+color.upper()
+print(color) # this doesnot change the original string as the upper method is only for temperary str 
+print(color.upper())
+style = " THis is Very Good Style"
+print(style.lower())
+print(style.replace("Good", "Perfect"))
+print(color.startswith("this"))
+print(style.endswith("yye"))
+print(color.find("the"))
+print(color.count("is"))
+
